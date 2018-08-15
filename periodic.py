cddb9e381d0950fb32f015f0fbb0bcf0d4c8449e
iammax <iammax64@gmail.com>
2018-08-15 15:10:48 -0400

    Added line to ensure results dir exists in xyx_matrix_maker and z_matrix_maker

--- periodic.py
+++ periodic.py
@@ -1,3 +1,4 @@
+#Add elements and their atomic number to this dict if you use other atoms. It's pretty sparse at the moment
 def periodic(name):
 	table = {'c':12, 'C':12, 'h': 1, 'H': 1, 'o': 16, 'O': 16, 'n': 14, 'N': 14}
 	if name in table:
